400e480dd7549d503e8a432c1a9eb2889b01f8a8
FrancoTobaldi-ItUp <franco.tobaldi@it-up.com.ar>
2025-09-20 19:04:38 -0300

    no message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'whatsapp-bot',
     script: 'server.js',
-    cwd: '/home/ubuntu/server-wp',
+    cwd: '/home/ubuntu/wp-messages-curl',
     env: {
       NODE_ENV: 'production',
       DISPLAY: ':99'
@@ -15,7 +15,6 @@ module.exports = {
     out_file: './logs/out.log',
     log_file: './logs/combined.log',
     time: true,
-    // Configuraciones específicas para venom-bot
     kill_timeout: 5000,
     wait_ready: true,
     listen_timeout: 10000,
